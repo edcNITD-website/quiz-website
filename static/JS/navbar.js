935b38c73ce1a0974c2d666c8239359658fd1898
Priyanshu <pb.21u10029@btech.nitdgp.ac.in>
2023-02-12 11:58:19 +0530

    Changes in navbar

--- static/JS/navbar.js
+++ static/JS/navbar.js
@@ -1,27 +1,8 @@
-(function ($) {
-  $(function () {
-    //  open and close nav
-    $("#navbar-toggle").click(function () {
-      $("nav ul").slideToggle();
-    });
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const navbar = document.getElementById("navbar");
 
-    // Hamburger toggle
-    $("#navbar-toggle").on("click", function () {
-      this.classList.toggle("active");
-    });
-
-    // If a link has a dropdown, add sub menu toggle.
-    $("nav ul li a:not(:only-child)").click(function (e) {
-      $(this).siblings(".navbar-dropdown").slideToggle("slow");
-
-      // Close dropdown when select another dropdown
-      $(".navbar-dropdown").not($(this).siblings()).hide("slow");
-      e.stopPropagation();
-    });
-
-    // Click outside the dropdown will remove the dropdown class
-    $("html").click(function () {
-      $(".navbar-dropdown").hide();
-    });
-  });
-})(jQuery);
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+  navbar.classList.toggle("bg_image");
+});
